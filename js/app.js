0b0d3985860b166a37423f537dc75368657c83f8
DownBlue <113776652+DownBlue@users.noreply.github.com>
2022-12-23 22:43:13 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+prompt (" Pas de problèmes pour venir jusqu'ici? ")
+
+
+  window.addEventListener('keyup', handleKeyUp)
+
+  var paragraphe = document.getElementById("paragraphe"); 
+
+  var pseudo = prompt('Entrez votre nom/pseudo')
+
+paragraphe.innerHTML += 'Salut <b>' + pseudo + "</b> moi c'est Théo Chabat, je suis étudiant à l'IIM en première annéeet je souhaiterai en A2 aller en game Art. Passioné par l'art depuis que je suis gamin j'ai décidé de poursuivre ce rêve après mon bac en allant dans des études de design numérique et plus tard en étant chara designer. Comme première axe l'année prochaine je demanderai Game Art et en second choix ça sera sûrement Création & Design qui reste toujours dans la création graphique bien que l'animation 3D pourrait m'être bénéfique pour le développement de mes capacités de modélisation et de chara design. Le point très positif de l'IIM est la première année qui permet de voir les 5 axes proposées, explorer tous les corps de métiers qui sont relié permet une vue d'ensemble ainsi qu'une acquisition de compétence dans divers domaine avec l'apport des bases des 5 axes.";
+
